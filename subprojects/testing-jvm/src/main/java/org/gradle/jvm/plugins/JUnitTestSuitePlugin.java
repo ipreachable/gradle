7806b31dfc72f43fb8c63f14aeece44491a3c5a0
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2015-12-16 23:05:06 -0200

    Reuse `testSuites` container for the JVM SM
     - Extract `TestingModelBasePlugin` from `NativeBinariesTestPlugin`
     - Create non buildable binaries for test suites declared under `testSuites`
     - Preserve current behavior for test suites declared under `components`
     - Some polishing (to be committed separately later):
       - Introduce explaining methods
       - DRY
       - Remove redundant `public` modifier

--- subprojects/testing-jvm/src/main/java/org/gradle/jvm/plugins/JUnitTestSuitePlugin.java
+++ subprojects/testing-jvm/src/main/java/org/gradle/jvm/plugins/JUnitTestSuitePlugin.java
@@ -33,12 +33,14 @@ import org.gradle.jvm.test.internal.DefaultJUnitTestSuiteBinarySpec;
 import org.gradle.jvm.test.internal.DefaultJUnitTestSuiteSpec;
 import org.gradle.language.base.DependentSourceSet;
 import org.gradle.language.base.LanguageSourceSet;
+import org.gradle.language.base.plugins.TestingModelBasePlugin;
 import org.gradle.language.java.JavaSourceSet;
 import org.gradle.language.java.tasks.PlatformJavaCompile;
 import org.gradle.language.jvm.JvmResourceSet;
 import org.gradle.model.*;
 import org.gradle.platform.base.*;
 import org.gradle.platform.base.internal.*;
+import org.gradle.platform.base.test.TestSuiteContainer;
 
 import java.io.File;
 import java.util.Collections;
@@ -51,7 +53,15 @@ import java.util.Set;
  * @since 2.11
  */
 @Incubating
-public class JUnitTestSuitePlugin extends RuleSource {
+public class JUnitTestSuitePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+        project.getPluginManager().apply(TestingModelBasePlugin.class);
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    static class Rules extends RuleSource {
 
         @ComponentType
         public void register(ComponentTypeBuilder<JUnitTestSuiteSpec> builder) {
@@ -66,12 +76,26 @@ public class JUnitTestSuitePlugin extends RuleSource {
             builder.internalView(JvmBinarySpecInternal.class);
         }
 
+// Only necessary if we decide NOT to add the binaries to the top level binaries container.
+//
+//        @Mutate
+//        public void createTestSuiteTasks(final ModelMap<Task> tasks,
+//                                         TestSuiteContainer testSuites,
+//                                         FileOperations fileOperations,
+//                                         @Path("buildDir") File buildDir) {
+//            for (JUnitTestSuiteSpec testSuite : testSuites.withType(JUnitTestSuiteSpec.class)) {
+//                for (JUnitTestSuiteBinarySpec binary : testSuite.getBinaries().withType(JUnitTestSuiteBinarySpec.class)) {
+//                    createTestSuiteTask(tasks, binary, fileOperations, buildDir);
+//                }
+//            }
+//        }
+
         @BinaryTasks
         void createTestSuiteTask(final ModelMap<Task> tasks,
                                  final JUnitTestSuiteBinarySpec binary,
                                  final FileOperations fileOperations,
                                  final @Path("buildDir") File buildDir) {
-            String taskName = ((BinarySpecInternal)binary).getProjectScopedName() + "Test";
+            String taskName = ((BinarySpecInternal) binary).getProjectScopedName() + "Test";
             final JvmAssembly jvmAssembly = ((WithJvmAssembly) binary).getAssembly();
 
             tasks.create(taskName, Test.class, new Action<Test>() {
@@ -107,30 +131,46 @@ public class JUnitTestSuitePlugin extends RuleSource {
             testTasks.all(new Action<Test>() {
                 @Override
                 public void execute(Test test) {
-                    UnionFileCollection testClasspath = new UnionFileCollection(test.getClasspath());
-                    Set<? extends Task> tasks = test.getTaskDependencies().getDependencies(test);
-                    for (Task task : tasks) {
-                        if (task instanceof PlatformJavaCompile) {
-                            FileCollection cp = ((PlatformJavaCompile) task).getClasspath();
-                            if (cp != null) {
-                                testClasspath.add(cp);
-                            }
-                        }
-                    }
-
-                    test.setClasspath(testClasspath);
+                    test.setClasspath(classpathFor(test));
                 }
             });
         }
 
+        /**
+         * Create binaries for test suites declared under {@code testSuites}.
+         *
+         * These are not built as part of {@code assemble}.
+         */
+        @Mutate
+        void createJUnitTestSuiteBinaries(TestSuiteContainer testSuites, final PlatformResolvers platformResolvers) {
+            testSuites.afterEach(JUnitTestSuiteSpec.class, new Action<JUnitTestSuiteSpec>() {
+                @Override
+                public void execute(JUnitTestSuiteSpec jUnitTestSuiteSpec) {
+                    createJUnitBinariesFor(jUnitTestSuiteSpec, false, platformResolvers, jUnitTestSuiteSpec.getBinaries());
+                }
+            });
+        }
+
+        /**
+         * Create binaries for test suites declared under {@code components}.
+         *
+         * These are built as part of {@code assemble}.
+         */
         @ComponentBinaries
-        void createJUnitBinaries(final ModelMap<JUnitTestSuiteBinarySpec> testBinaries, final PlatformResolvers platformResolver, final JUnitTestSuiteSpec testSuite) {
+        void createJUnitComponentBinaries(final ModelMap<BinarySpec> testBinaries, final PlatformResolvers platformResolver, final JUnitTestSuiteSpec testSuite) {
+            createJUnitBinariesFor(testSuite, true, platformResolver, testBinaries);
+        }
+
+        private void createJUnitBinariesFor(final JUnitTestSuiteSpec testSuite, final boolean buildable, PlatformResolvers platformResolver, ModelMap<BinarySpec> testBinaries) {
             final List<JavaPlatform> javaPlatforms = resolvePlatforms(platformResolver);
             final JavaPlatform platform = javaPlatforms.get(0);
             final BinaryNamingScheme namingScheme = namingSchemeFor(testSuite, javaPlatforms, platform);
             testBinaries.create(namingScheme.getBinaryName(), JUnitTestSuiteBinarySpec.class, new Action<JUnitTestSuiteBinarySpec>() {
+
                 @Override
                 public void execute(JUnitTestSuiteBinarySpec jUnitTestSuiteBinarySpec) {
+                    ((BinarySpecInternal) jUnitTestSuiteBinarySpec).setBuildable(buildable);
+
                     final String jUnitVersion = testSuite.getJUnitVersion();
                     ((BinarySpecInternal) jUnitTestSuiteBinarySpec).setNamingScheme(namingScheme);
                     jUnitTestSuiteBinarySpec.setJUnitVersion(jUnitVersion);
@@ -142,6 +182,7 @@ public class JUnitTestSuitePlugin extends RuleSource {
                             // For now, dependencies have to be defined at the source set level
                             // in order for the dependency resolution engine to kick in
                             if (languageSourceSet instanceof DependentSourceSet) {
+                                // TODO:RBO consider adding this to the testSuite dependencies container
                                 ((DependentSourceSet) languageSourceSet).getDependencies().group("junit").module("junit").version(jUnitVersion);
                             }
                         }
@@ -150,30 +191,59 @@ public class JUnitTestSuitePlugin extends RuleSource {
             });
         }
 
+        /**
+         * Add default source sets to test suites declared under {@code components}.
+         */
         @Defaults
-        void addDefaultSourceSets(ModelMap<JUnitTestSuiteSpec> suites) {
+        void addDefaultSourceSetsToTestSuiteComponents(@Path("components") ModelMap<JUnitTestSuiteSpec> components) {
+            addDefaultSourceSetsTo(components);
+        }
+
+        /**
+         * Add default source sets to test suites declared under {@code testSuites}.
+         */
+        @Defaults
+        void addDefaultSourceSetsToTestSuites(@Path("testSuites") ModelMap<JUnitTestSuiteSpec> testSuites) {
+            addDefaultSourceSetsTo(testSuites);
+        }
+
+        private void addDefaultSourceSetsTo(ModelMap<JUnitTestSuiteSpec> suites) {
             suites.all(new Action<JUnitTestSuiteSpec>() {
                 @Override
                 public void execute(final JUnitTestSuiteSpec jUnitTestSuiteSpec) {
-                    jUnitTestSuiteSpec.getSources().create("java", JavaSourceSet.class, new Action<JavaSourceSet>() {
-                        @Override
-                        public void execute(JavaSourceSet languageSourceSet) {
-                            languageSourceSet.getSource().srcDir(String.format("src/%s/java", jUnitTestSuiteSpec.getName()));
-                        }
-                    });
-                    jUnitTestSuiteSpec.getSources().create("resources", JvmResourceSet.class, new Action<JvmResourceSet>() {
-                        @Override
-                        public void execute(JvmResourceSet languageSourceSet) {
-                            languageSourceSet.getSource().srcDir(String.format("src/%s/resources", jUnitTestSuiteSpec.getName()));
-                        }
-                    });
+                    addSourceSetTo(jUnitTestSuiteSpec, "java", JavaSourceSet.class);
+                    addSourceSetTo(jUnitTestSuiteSpec, "resources", JvmResourceSet.class);
                 }
             });
         }
 
+        private void addSourceSetTo(final JUnitTestSuiteSpec spec, final String sourceSetName, Class<? extends LanguageSourceSet> type) {
+            spec.getSources().create(sourceSetName, type, new Action<LanguageSourceSet>() {
+                @Override
+                public void execute(LanguageSourceSet languageSourceSet) {
+                    languageSourceSet.getSource().srcDir(String.format("src/%s/%s", spec.getName(), sourceSetName));
+                }
+            });
+        }
+
+        /**
+         * Validate test suites declared under {@code components}.
+         */
         @Validate
-        void validateJUnitTestSuites(ModelMap<JUnitTestSuiteSpec> suites) {
-            suites.all(new Action<JUnitTestSuiteSpec>() {
+        void validateJUnitTestSuiteComponents(@Path("components") ModelMap<JUnitTestSuiteSpec> components) {
+           validateJUnitTestSuitesIn(components);
+        }
+
+        /**
+         * Validate test suites declared under {@code testSuites}.
+         */
+        @Validate
+        void validateJUnitTestSuites(@Path("testSuites") ModelMap<JUnitTestSuiteSpec> testSuites) {
+            validateJUnitTestSuitesIn(testSuites);
+        }
+
+        private void validateJUnitTestSuitesIn(ModelMap<JUnitTestSuiteSpec> testSuites) {
+            testSuites.all(new Action<JUnitTestSuiteSpec>() {
                 @Override
                 public void execute(JUnitTestSuiteSpec jUnitTestSuiteSpec) {
                     if (jUnitTestSuiteSpec.getJUnitVersion() == null) {
@@ -196,5 +266,18 @@ public class JUnitTestSuitePlugin extends RuleSource {
                 .withVariantDimension(platform, selectedPlatforms);
         }
 
-
+        private FileCollection classpathFor(Test test) {
+            UnionFileCollection testClasspath = new UnionFileCollection(test.getClasspath());
+            Set<? extends Task> tasks = test.getTaskDependencies().getDependencies(test);
+            for (Task task : tasks) {
+                if (task instanceof PlatformJavaCompile) {
+                    FileCollection cp = ((PlatformJavaCompile) task).getClasspath();
+                    if (cp != null) {
+                        testClasspath.add(cp);
+                    }
+                }
+            }
+            return testClasspath;
+        }
+    }
 }
